3e359a0173d14d45ce310601fe59a375b2e4354f
onowakow <nowakowskiowen08@gmail.com>
2023-06-10 18:03:06 -0600

    Mostly through writing article on generating polyline

--- docusaur/docusaurus.config.js
+++ docusaur/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://www.onowak.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -41,8 +41,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://dev.azure.com/onowakow/_git/Blog?path=/docusaur',
         },
         blog: {
           blogTitle: 'Blog',
